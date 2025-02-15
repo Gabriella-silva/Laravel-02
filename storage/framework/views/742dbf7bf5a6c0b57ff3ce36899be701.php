f383461862008e17fc0e56be41b5dd48d64937e8
Gabriella Ferreira Silva <165532062+Gabriella-silva@users.noreply.github.com>
2025-02-14 22:43:01 -0300

    <3

---
+++ storage/framework/views/742dbf7bf5a6c0b57ff3ce36899be701.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>GAY</h3>
+
+    <div class="container">
+   <center> <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/5FVM8teszzq7kZyIjkI4Vu?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></center>
+    </div>
+    <style>
+        .container{
+
+        }
+     </style>
+</body>
+</html><?php /**PATH C:\Users\Alunos\Desktop\Webapp\teste-app\resources\views/olamundo.blade.php ENDPATH**/ ?>
